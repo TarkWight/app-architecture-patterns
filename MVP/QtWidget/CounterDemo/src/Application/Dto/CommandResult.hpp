84b5a3a8f3bad96c500749305cb6bef5bb727e15
TarkWight <arsentiy.gonda@gmail.com>
2026-02-16 23:41:50 +0700

    application::dto: add command result dto

---
+++ MVP/QtWidget/CounterDemo/src/Application/Dto/CommandResult.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMANDRESULT_HPP
+#define COMMANDRESULT_HPP
+
+namespace application::dto {
+
+struct CommandResult final {
+    int newValue{0};
+};
+
+} // namespace application::dto
+
+#endif // COMMANDRESULT_HPP

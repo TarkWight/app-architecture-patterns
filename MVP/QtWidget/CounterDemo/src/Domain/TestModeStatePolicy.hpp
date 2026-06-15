bc90094701773c542850fb0b850a54c349e26beb
TarkWight <arsentiy.gonda@gmail.com>
2026-06-15 11:41:15 +0700

    Move mode state policy into domain

---
+++ MVP/QtWidget/CounterDemo/src/Domain/TestModeStatePolicy.hpp
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "StandControlMode.hpp"
+#include "TestTimeDirection.hpp"
+#include "TestTimeSource.hpp"
+
+namespace domain {
+
+struct TestModeState {
+    TestMode testMode{TestMode::Manual};
+    StandControlMode standControlMode{StandControlMode::Manual};
+    TestTimeSource timeSource{TestTimeSource::FreeRun};
+    TestTimeDirection timeDirection{TestTimeDirection::CountUp};
+};
+
+class TestModeStatePolicy final {
+  public:
+    [[nodiscard]] static TestModeState fromStandControlMode(StandControlMode mode) {
+        return fromTestMode(testModeForStandControlMode(mode));
+    }
+
+    [[nodiscard]] static TestModeState fromTestMode(TestMode mode) {
+        const auto standMode = standControlModeForTestMode(mode);
+
+        switch (mode) {
+        case TestMode::Manual:
+            return TestModeState{.testMode = mode,
+                                 .standControlMode = standMode,
+                                 .timeSource = TestTimeSource::FreeRun,
+                                 .timeDirection = TestTimeDirection::CountUp};
+        case TestMode::Hybrid:
+        case TestMode::Automatic:
+            return TestModeState{.testMode = mode,
+                                 .standControlMode = standMode,
+                                 .timeSource = TestTimeSource::AutoCalculated,
+                                 .timeDirection = TestTimeDirection::CountDown};
+        }
+
+        return TestModeState{};
+    }
+
+    [[nodiscard]] static TestTimeDirection directionForTimeSource(TestTimeSource source) {
+        switch (source) {
+        case TestTimeSource::AutoCalculated:
+        case TestTimeSource::OperatorDefined:
+            return TestTimeDirection::CountDown;
+        case TestTimeSource::FreeRun:
+            return TestTimeDirection::CountUp;
+        }
+
+        return TestTimeDirection::CountUp;
+    }
+};
+
+} // namespace domain
